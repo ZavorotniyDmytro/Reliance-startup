ae8e6a3397fb970ed3284995f30e6adaa1570089
Zavorotniy Dmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-04-08 02:43:58 +0300

    designing routing

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -22,7 +22,8 @@ export class UserController {
 		private readonly userService: UserService,
 		private readonly resumeService: ResumeService
 	) { }
-
+	
+	// GET /users - повертає список всіх користувачів
 	@ApiTags("User API")
 	@ApiOperation({ summary: "Get all users" })
 	@ApiResponse({ status: 200, type: [User] })
@@ -31,6 +32,7 @@ export class UserController {
 		return this.userService.getAll();
 	}
 
+	// GET /users/{id} - повертає конкретного користувача за його ідентифікатором
 	@ApiTags("User API")
 	@ApiOperation({ summary: "Get user by ID" })
 	@ApiResponse({ status: 200, type: User })
@@ -39,6 +41,7 @@ export class UserController {
 		return this.userService.getById(id);
 	}
 
+	// POST /users - створює нового користувача
 	@ApiTags("User API")
 	@ApiOperation({ summary: "Create user" })
 	@ApiResponse({ status: 201, type: User })
@@ -47,6 +50,7 @@ export class UserController {
 		return this.userService.create(data);
 	}
 
+	// PUT /users/{id} - оновлює конкретного користувача за його ідентифікатором
 	@ApiTags("User API")
 	@ApiOperation({ summary: "Update user by ID" })
 	@ApiResponse({ status: 200, type: Promise<User> })
@@ -55,6 +59,7 @@ export class UserController {
 		return this.userService.update(id, data);
 	}
 
+	// DELETE /users/{id} - видаляє конкретного користувача за його ідентифікатором
 	@ApiTags("User API")
 	@ApiOperation({ summary: "Delete user by ID" })
 	@ApiResponse({ status: 200, type: Promise<void> })
@@ -63,48 +68,79 @@ export class UserController {
 		this.userService.delete(id);
 	}
 
+	// GET /users/{user_id}/announcements - повертає список всіх оголошень, створених конкретним користувачем
 	@ApiTags("User API")
 	@ApiOperation({ summary: "Get all announcement by user ID" })
 	@ApiResponse({ status: 200, type: Announcement })
 	@Get(':user_id/announcements')
 	getAllAnnouncement(@Param('user_id') user_id: string) {
-		return this.userService.getAllAnnouncement(user_id);
+		// TODO update this code. Rewrite getAllAnnouncements in userService
+		return this.userService.getAllAnnouncements(user_id);
 	}
 
+	// POST /users/{user_id}/resumes - створює нове резюме для конкретного користувача
 	@ApiTags("Resume API")
 	@ApiOperation({ summary: "Create resume by user ID" })
 	@ApiResponse({ status: 200, type: Resume })
-	@Post(':user_id/resume')
+	@Post(':user_id/resumes')
 	async createResume(@Param('user_id') user_id: number, @Body() data: ResumeDto) {
+		// TODO update this code
 		const user_data = await this.userService.getDataForResume(user_id);
 		const resume_data = ({ ...data, user_id, ...user_data });
 		return this.resumeService.create(resume_data);
 	}
 
+	// GET /users/{user_id}/resumes/{id} - повертає конкретне резюме конкретного користувача за його ідентифікатором
 	@ApiTags("Resume API")
 	@ApiOperation({ summary: "Get resume" })
 	@ApiResponse({ status: 200, type: [Resume] })
-	@Get(':user_id/resume/:id')
+	@Get(':user_id/resumes/:id')
 	getResumeById(@Param('id') id: number): Promise<Resume> {
+		// TODO check resumeService
 		return this.resumeService.getById(id);
 	}
 
+	// PUT /users/{user_id}/resumes/{id} - оновлює конкретне резюме конкретного користувача за його ідентифікатором
 	@ApiTags("Resume API")
 	@ApiOperation({ summary: "Update resume by Id" })
 	@ApiResponse({ status: 203, type: ResumeDto })
-	@Put(':user_id/resume/:id')
+	@Put(':user_id/resumes/:id')
 	async updateResume(@Param('id') id: number, @Body() data: Partial<Resume>): Promise<Resume> {
+		// TODO check resumeService
 		return this.resumeService.update(id, data);
 	}
 
+	// DELETE /users/{user_id}/resumes/{id} - видаляє конкретне резюме конкретного користувача за його ідентифікатором
 	@ApiTags("Resume API")
 	@ApiOperation({ summary: "Delete resume by Id" })
 	@ApiResponse({ status: 204, type: Resume })
-	@Delete(':user_id/resume/:id')
+	@Delete(':user_id/resumes/:id')
 	async deleteResume(@Param('id') id: number): Promise<void> {
+		// TODO check resumeService
 		this.resumeService.delete(id);
 	}
 
-	// get :user_id/chats
+	// GET /users/{user_id}/contracts/employer - повертає список всіх контрактів, пов'язаних з конкретним користувачем (роботодавцем)
+	// GET /users/{user_id}/contracts/worker - повертає список всіх контрактів, пов'язаних з конкретним користувачем (Робітником)
+
+	// GET /users/{user_id}/chats - повертає список всіх чатів, пов'язаних з конкретним користувачем
+	// GET /users/{user_id}/messages - повертає список всіх повідомлень, пов'язаних з конкретним користувачем
+	// GET /users/{user_id}/reviews - повертає список всіх відгуків, пов'язаних з конкретним користувачем
+	
+	// TODO the announcement must not have a controller, only module and service
+	// GET /users/{user_id}/announcements - повертає список всіх оголошень, створених конкретним користувачем
+	// POST /users/{user_id}/announcements - створює нове оголошення для конкретного користувача
+	// GET /users/{user_id}/announcements/{id} - повертає конкретне оголошення конкретного користувача за його ідентифікатором
+	// PUT /users/{user_id}/announcements/{id} - оновлює конкретне оголошення конкретного користувача за його ідентифікатором
+	// DELETE /users/{user_id}/announcements/{id} - видаляє конкретне оголошення конкретного користувача за його ідентифікаторо
+	
+	// GET /users/{user_id}/announcements/{id}/followers - повертає список всіх підписників конкретного оголошення
+	// POST /users/{user_id}/announcements/{id}/followers - додає нового підписника до списку підписників конкретного оголошення
+	// GET /users/{user_id}/announcements/{id}/followers/{id} - повертає конкретного підписника конкретного оголошення за його ідентифікатором
+	// PUT /users/{user_id}/announcements/{id}/followers/{id} - оновлює конкретного підписника конкретного оголошення за його ідентифікатором
+	// DELETE /users/{user_id}/announcements/{id}/followers/{id} - видаляє конкретного підписника конкретного оголошення за його ідентифікатором
+
+
+	// 
 }
 
